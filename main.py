8ed02778045059bdb0d21367d5ffa5d508bc884b
King-Of-End <legobooster17@gmail.com>
2026-04-28 18:25:52 +0300

    промежуточная

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/member')
+def member():
+
+    return render_template('member.html',)
+
+if __name__ == '__main__':
+    app.run(port=8080)
