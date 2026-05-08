72eb33584bffb1893c346907c5853b1e9597e2a0
King-Of-End <legobooster17@gmail.com>
2026-05-08 18:33:32 +0300

    Обработчик формы авторизации

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
-from data.db_session import global_init, create_session
-from models import User, Jobs
-
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_wtf import FlaskForm
 from sqlalchemy import select
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
-global_init(input())
-sess = create_session()
-resp = sess.execute(select(Jobs)).scalars()
-k = max(len(x.collaborators.split(', ')) for x in resp)
-for i in resp:
-    if len(i.collaborators.split(', ')) == k:
-        print(i.team_leader_obj)
+from data.db_session import create_session, global_init
+from models import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = create_session()
+    return db_sess.get(User, user_id)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = create_session()
+        user_select = select(User).where(User.email == form.email.data)
+        user = db_sess.execute(user_select).scalar()
+        print(user.check_password(form.password.data))
+        print(form.password.data)
+        print(user.hashed_password)
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+if __name__ == '__main__':
+    global_init('db/users_new.db')
+    app.run()
--- models/users.py
+++ models/users.py
@@ -1,9 +1,10 @@
 import sqlalchemy
+from flask_login import UserMixin
 
 from data.db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String)
@@ -15,3 +16,6 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime)
+
+    def check_password(self, password):
+        return self.hashed_password == password
